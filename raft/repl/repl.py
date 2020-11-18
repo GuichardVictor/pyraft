bcc8e71d06dc0d5d971a763a4f23cbdf3a71ab10
GuichardVictor <guichardvictor@gmail.com>
2020-11-18 14:20:15 +0100

    feat: change repl msg handling to msg factory

--- raft/repl/repl.py
+++ raft/repl/repl.py
@@ -1,7 +1,5 @@
 import cmd
 
-from ..messages.message import ReplMessage
-
 import logging
 
 logger = logging.getLogger(__name__)
@@ -10,17 +8,18 @@ class RaftRepl(cmd.Cmd):
     intro = '=== pyraft repl in to interact with created cluster. Type help or ? to list commands. ===\n'
     prompt = '(pyraft) >>> '
 
-    def __init__(self, cluster, transport):
+    def __init__(self, cluster, transport, message_factory):
         super().__init__(self)
         self.cluster = cluster
         self.transport = transport
+        self.message_factory = message_factory
 
     def do_SPEED(self, arg : str):
         parsed = arg.split()
         receiver = int(parsed[0])
         speed = parsed[1]
         
-        msg = ReplMessage.SpeedMessage('repl', receiver, speed)
+        msg = self.message_factory.SpeedMessage('repl', receiver, speed)
         self.transport.sendo(msg, receiver)
 
         logger.info(f'[REPL] sending SPEED("{speed}") message to {receiver}.')
@@ -30,7 +29,7 @@ class RaftRepl(cmd.Cmd):
         parsed = arg.strip()
         receiver = int(parsed)
 
-        msg = ReplMessage.CrashMessage('repl', receiver)
+        msg = self.message_factory.CrashMessage('repl', receiver)
         self.transport.sendo(msg, receiver)
 
         logger.info(f'[REPL] sending CRASH message to {receiver}.')
@@ -38,7 +37,7 @@ class RaftRepl(cmd.Cmd):
     def do_START(self, arg : str):
         logger.info('[REPL] sending START message.')
 
-        msg = ReplMessage.StartMessage('repl', None)
+        msg = self.message_factory.StartMessage('repl', None)
         for receiver in self.cluster:
             msg.receiver = receiver
             self.transport.sendto(msg, receiver)
